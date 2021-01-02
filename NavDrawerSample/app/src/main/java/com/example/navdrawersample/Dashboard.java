407c8e343e5cbb3bd971fad72c007f38b623b6c8
Yogesh Arroju <yogesh.arroju@temenos.com>
2021-01-02 20:55:37 +0530

    intial commit for navBar without fragments

---
+++ NavDrawerSample/app/src/main/java/com/example/navdrawersample/Dashboard.java
@@ -0,0 +1,52 @@
+package com.example.navdrawersample;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import static com.example.navdrawersample.MainActivity.closeDrawer;
+
+public class Dashboard extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+
+    @Override
+     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    }
+    public void ClickMenu(View view){
+
+        MainActivity.openDrawer(drawerLayout);
+    }
+
+    public void ClickLogo(View view){
+
+        closeDrawer(drawerLayout);
+    }
+
+    public void ClickHome(View view){
+
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickDashboard(View view){
+        recreate();
+        closeDrawer(drawerLayout);
+
+    }
+
+    public void ClickAboutUs(View view){
+
+        MainActivity.redirectActivity(this,AboutUs.class);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeDrawer(drawerLayout);
+    }
+}
---
+++ NavDrawerSample/app/src/main/java/com/example/navdrawersample/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.navdrawersample;
+
+import android.app.Activity;
+import android.content.Intent;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity<pulbic> extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+    }
+
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+
+    public static void  closeDrawer(DrawerLayout drawerLayout) {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickHome(View view){
+        recreate();
+        closeDrawer(drawerLayout);
+    }
+
+    public void ClickDashboard(View view){
+        redirectActivity(this,Dashboard.class);
+    }
+
+    public void ClickAboutUs(View view){
+        redirectActivity(this,AboutUs.class);
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        Intent intent = new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+}
